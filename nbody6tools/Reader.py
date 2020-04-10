69d9ff345b2d89421b40a37f0c2c862c7b9afe56
Juan Farias <juanfariaso@gmail.com>
2020-04-10 13:58:46 +0200

    update Reader.get_times Reader.globals returns a dict

--- nbody6tools/Reader.py
+++ nbody6tools/Reader.py
@@ -22,9 +22,14 @@ def get_globals(folder):
     "find last block of globals that is good"
     x = open("global.30","r").readlines()
     it = [ i for i in range(len(x)) if "TIME" in x[i] ]  #find headers if more than one
-    header = x[it[-1]] 
-    data = x[it[-1]+1:] 
-    return data
+    #header = x[it[-1]] 
+    header = "TIME[NB] TIME[Myr] TCR[Myr] DE BE(3) RSCALE[PC] RTIDE[PC] RDENS[PC] RC[PC]  RHOD[M*] RHOM[M*] MC[M*] CMAX <Cn> Ir/R RCM VCM AZ EB/E EM/E VRMS N NS NPAIRS NUPKS NPKS NMERGE MULT <NB> NC NESC NSTEPI NSTEPB NSTEPR NSTEPU NSTEPT NSTEPQ NSTEPC NBLOCK NBLCKR NNPRED NIRRF NBCORR NBFLUX NBFULL NBVOID NICONV NLSMIN NBSMIN NBDIS NBDIS2 NCMDER NFAST NBFAST NKSTRY NKSREG NKSHYP NKSPER NKSMOD NTTRY NTRIP NQUAD NCHAIN NMERG NEWHI"
+    header = header.split()
+    data = numpy.loadtxt(x[it[-1]+1:] )
+    result = dict() 
+    for i,h in enumerate(header):
+        result[h] = data[:,i]
+    return result
 
 def parse_inputfile(inputfilename,**kw):
     """%s
@@ -32,15 +37,13 @@ def parse_inputfile(inputfilename,**kw):
     return Datamodel.parse_inputfile(inputfilename,**kw)
 
 def get_number_of_snapshots(folder,inputfilename=inputFile,**kw):
+    return len(get_times(folder,inputfilename))
+
+def get_times(folder,nbody=False,inputfilename=inputFile):
     glfile = get_globals(folder)
     inputfile = parse_inputfile(inputfilename)
-    return int(len(glfile)  / inputfile["NFIX"] ) + 1
-
-def get_times(folder,dtype=float):
-    l=[x.replace("%sconf.3_"%folder,"") for x in glob.glob(folder+"%s*"%snapshotFile) ]
-    l.sort(key=float)
-    l = numpy.array(l,dtype=dtype)
-    return l
+    key = "TIME[NB]" if nbody else "TIME[Myr]"
+    return glfile[key][0::inputfile["NFIX"] ]
 
 def read_snapshot(folder,snapshot=0,inputfilename=inputFile,singlefile=singleFile,
         snapshotfile = snapshotFile):
